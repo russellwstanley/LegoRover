96445a105437d7986cb4faa4160f1b7184b65188
russell <russell@russell-PORTEGE-R700.(none)>
2013-04-20 17:07:07 +0800

    drag and drop into scrollable panel

--- src/com/singapore/legorover/MainActivity.java
+++ src/com/singapore/legorover/MainActivity.java
@@ -1,16 +1,16 @@
 /*INSERT YOUR PACKAGE NAME*/
 package com.singapore.legorover;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.ClipData;
-import android.graphics.Typeface;
+import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.DragShadowBuilder;
 import android.view.View.OnDragListener;
 import android.view.View.OnTouchListener;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
@@ -23,7 +23,8 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	//text views being dragged and dropped onto
-	private TextView option1, option2, option3, choice1, choice2, choice3;
+	private TextView option1, option2, option3;
+	private LinearLayout dropzone;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +38,7 @@ public class MainActivity extends Activity {
 		option2 = (TextView)findViewById(R.id.option_2);
 		option3 = (TextView)findViewById(R.id.option_3);
 
-		//views to drop onto
-		choice1 = (TextView)findViewById(R.id.choice_1);
-		choice2 = (TextView)findViewById(R.id.choice_2);
-		choice3 = (TextView)findViewById(R.id.choice_3);
+		dropzone = (LinearLayout)findViewById(R.id.dropzone);
 
 		//set touch listeners
 		option1.setOnTouchListener(new ChoiceTouchListener());
@@ -48,9 +46,7 @@ public class MainActivity extends Activity {
 		option3.setOnTouchListener(new ChoiceTouchListener());
 
 		//set drag listeners
-		choice1.setOnDragListener(new ChoiceDragListener());
-		choice2.setOnDragListener(new ChoiceDragListener());
-		choice3.setOnDragListener(new ChoiceDragListener());
+		dropzone.setOnDragListener(new ChoiceDragListener());
 	}
 
 	/**
@@ -98,29 +94,14 @@ public class MainActivity extends Activity {
 				break;
 			case DragEvent.ACTION_DROP:
 				//handle the dragged view being dropped over a drop view
-				View view = (View) event.getLocalState();
-				//stop displaying the view where it was before it was dragged
-				view.setVisibility(View.INVISIBLE);
+				TextView view = (TextView) event.getLocalState();
 				//view dragged item is being dropped on
-				TextView dropTarget = (TextView) v;
-				//view being dragged and dropped
-				TextView dropped = (TextView) view;
+				LinearLayout dropTarget = (LinearLayout) v;
+				TextView dropped = new TextView(MainActivity.this);
+				dropped.setText(view.getText());
+				
 				//update the text in the target view to reflect the data being dropped
-				dropTarget.setText(dropped.getText());
-				//make it bold to highlight the fact that an item has been dropped
-				dropTarget.setTypeface(Typeface.DEFAULT_BOLD);
-				//if an item has already been dropped here, there will be a tag
-				Object tag = dropTarget.getTag();
-				//if there is already an item here, set it back visible in its original place
-				if(tag!=null)
-				{
-					//the tag is the view id already dropped here
-					int existingID = (Integer)tag;
-					//set the original view visible again
-					findViewById(existingID).setVisibility(View.VISIBLE);
-				}
-				//set the tag in the target view being dropped on - to the ID of the view being dropped
-				dropTarget.setTag(dropped.getId());
+				dropTarget.addView(dropped);
 				break;
 			case DragEvent.ACTION_DRAG_ENDED:
 				//no action necessary
